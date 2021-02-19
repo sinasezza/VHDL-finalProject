88c7a86141d6b78619f9b6d8c2651e46410c2ed2
sinasezza <s.e.sezza121@gmai.com>
2021-02-19 22:55:59 +0330

    this is fist commit : creating Main module and making its arrangement.

---
+++ Main.vhd
@@ -0,0 +1,19 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+
+ENTITY Main IS
+
+END ENTITY;
+
+-----------------------------------------------------
+-----------------------------------------------------
+-----------------------------------------------------
+
+ARCHITECTURE Main_ARCH OF Main IS
+------------------------------------DECLARATION BEGIN
+
+
+------------------------------------END DECLARATION
+BEGIN
+
+END ARCHITECTURE;
